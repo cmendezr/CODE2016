c06b2b4fbe6a027ccb6fc9721f45f11ab959013e
Carlos Mendez Ramirez <carlos.mendez@utng.edu.mx>
2016-03-07 18:14:51 -0600

    version actual del proyecto

--- CODEPROYECTO2016/ViewController.m
+++ CODEPROYECTO2016/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // se agregan lineas para revisar el commit
 }
 
 - (void)didReceiveMemoryWarning {
